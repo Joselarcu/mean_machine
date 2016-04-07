4cb6ff4a8e8e4d3d88ed738582dda6f25fbf0847
Jose Vicente Lara Cuadrado <jvlara@ext.aqualogybs.net>
2016-04-07 16:57:58 +0200

    getAll users

--- server.js
+++ server.js
@@ -92,6 +92,16 @@ apiRouter.route('/users')
  	});
 })
 
+// get all the users (accessed at GET http://localhost:8080/api/users)
+.get(function(req, res) {
+	User.find(function(err, users) {
+	if (err) res.send(err);
+
+	// return the users
+	res.json(users);
+	});
+});
+
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', apiRouter);
